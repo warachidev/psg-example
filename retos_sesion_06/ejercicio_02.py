c338370578d52553ff81231e60ab52dc12bd0069
alex_laptop <alexander65.af@gmail.com>
2024-05-10 11:21:27 -0400

    Sesion 6

---
+++ retos_sesion_06/ejercicio_02.py
@@ -0,0 +1,4 @@
+print (not (True or True) or (True and True))
+print (not (True or False) or (True and False))
+print (not (False or True) or (False and True))
+print (not (False or False) or (False and False))
