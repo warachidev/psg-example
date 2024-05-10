f93104828b3a596082957a53bdf33f93342fd364
Alex Flores_escritorio <alexander65.af@gmail.com>
2024-05-09 21:47:15 -0400

    Ejercicio 2 de la Sesión 3

---
+++ retos_sesion_03/ejercicio_02.py
@@ -0,0 +1,12 @@
+print ("Carlos: Hola")
+print ("María: Hola Carlos, ¿Ya comiste?")
+print ("Carlos: Claro. Comi una pizza vegetariana")
+print ("María: Enserio? y que tal estuvo")
+print ("Carlos: Exiquisita, te recomiendo que te pidas una")
+print ("María: Esta bien, lo hare gracias por la recomendacion")
+print ("Carlos: Cuando la pruebes cuentame si te gusto")
+print ("Carlos: Nos vemos luego")
+print ("María: Ok, te lo contare. Adios")
+
+
+
